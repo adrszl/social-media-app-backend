56aad4f0c5b761a128bb2a70c06b3bc0505acace
Adrian Szlegel <adrian@oaknode.com>
2020-02-15 23:52:57 +0100

    Added functions for getting and setting the posts & added express & added "api" prefix to the endpoint

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams = [
+        {
+            userHandle: 'user',
+            body: 'Body of the post',
+            createdAt: '2020-02-15T22:49:59.741Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,61 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+admin.initializeApp();
+
+// 
+// GET POSTS function
+// 
+app.get('/screams', (request, response) => {
+    admin.firestore().collection('screams').orderBy('createdAt', 'desc').get()
+        .then(data => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: DOMRectList.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
+            });
+            return response.json(screams);
+        })
+        .catch(err => console.error(err));
 });
+
+// 
+// HELLO WORLD function
+// 
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+// 
+// SET NEW POST function
+// 
+app.post('/scream', (request, response) => {
+    const newScream = {
+        body: request.body.body,
+        userHandle: request.body.userHandle,
+        createdAt: new Date().toISOString()
+    };
+
+    admin
+        .firestore()
+        .collection('screams')
+        .add(newScream)
+        .then((doc) => {
+            response.json({ message: `document ${doc.id} created successfully` });
+        })
+        .catch((err) => {
+            response.status(500).json({ error: 'something went wrong...' });
+            console.log(error);
+        });
+});
+
+//
+// PREFIX "API" DO ENDPOINTÓW
+//
+exports.api = functions.https.onRequest(app);
